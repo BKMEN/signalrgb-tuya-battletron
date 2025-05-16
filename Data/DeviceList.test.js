2ffc8e6b34efd65ed9cbc7956e001731ec3dd843
BKMEN <91731848+BKMEN@users.noreply.github.com>
2025-05-16 21:13:42 +0200

    Update DeviceList.test.js

--- Data/DeviceList.test.js
+++ Data/DeviceList.test.js
@@ -1,38 +1,9 @@
 export default {
-    // Dispositivos por defecto (comentados)
-    // "083b8r848jmkevue": { leds: [1,2,3] },
-    // "rgh3w8qrucy3ymcj": { leds: [1,2,3] },
-    // ...
-
-    // Dispositivos Battletron
-    "stmkcsykq3kheboa": {
-        leds: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
-        name: "Battletron Light Bar"
-    },
-    "yrg6a649fkozp1hw": {
-        leds: [1],
-        name: "Battletron Ball Light"
-    },
-
-    // Tus dispositivos personalizados (versión estable con 4 LEDs)
-    "bfafad43febddb888apxbj": {
-        leds: [1,2,3,4],
-        name: "Monitor"
-    },
-    "bfbebb82be7220f985rawa": {
-        leds: [1,2,3,4],
-        name: "Escritorio"
-    },
-    "bfde5007394a05833ahsda": {
-        leds: [1,2,3,4],
-        name: "Leds habitación 1"
-    },
-    "bfbe7bd231444751090bsq": {
-        leds: [1,2,3,4],
-        name: "Leds habitación 2"
-    },
-    "bfc51f8e2052b74f49nvd4": {
-        leds: [1,2,3,4],
-        name: "Luces Ali"
-    }
-};
+    "stmkcsykq3kheboa": { leds: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], name: "Battletron Light Bar" },
+    "yrg6a649fkozp1hw": { leds: [1], name: "Battletron Ball Light" },
+    "bfbebb82be7220f985rawa": { leds: [1,2,3,4], name: "Tuya Strip 1" },
+    "bfde5007394a05833ahsda": { leds: [1,2,3,4], name: "Tuya Strip 2" },
+    "bfbe7bd231444751090bsq": { leds: [1,2,3,4], name: "Tuya Strip 3" },
+    "bfafad43febddb888apxbj": { leds: [1,2,3,4], name: "Tuya Strip 4" },
+    "bfc51f8e2052b74f49nvd4": { leds: [1,2,3,4], name: "Luces Ali" },
+}
