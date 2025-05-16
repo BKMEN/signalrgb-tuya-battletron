c1419e426435dedae5a1f4ff2ba7af5aff2f1cb1
BKMEN <91731848+BKMEN@users.noreply.github.com>
2025-05-16 22:48:36 +0200

    Update TuyaVirtualDevice.test.js

--- TuyaVirtualDevice.test.js
+++ TuyaVirtualDevice.test.js
@@ -39,7 +39,7 @@ export default class TuyaVirtualDevice extends BaseClass
     setupDevice(tuyaDevice)
     {
         this.tuyaLeds = DeviceList[tuyaDevice.deviceType].leds;
-        this.ledCount = (this.tuyaLeds.length > 4) ? 4 : this.tuyaLeds.length;
+        this.ledCount = this.tuyaLeds.length;
 
         this.ledNames = this.getLedNames();
         this.ledPositions = this.getLedPositions();
@@ -47,7 +47,6 @@ export default class TuyaVirtualDevice extends BaseClass
         device.setName(tuyaDevice.getName());
 
         device.setSize([this.ledCount, 1]);
-        // device.setScale(1);
         device.setControllableLeds(this.ledNames, this.ledPositions);
     }
 
@@ -63,7 +62,6 @@ export default class TuyaVirtualDevice extends BaseClass
                     RGBData = this.getDeviceRGB();
                     break;
                 case "Forced":
-                    // RGBData = [this.hexToRGB(forcedColor)];
                     for (let i = 0; i < this.ledCount; i++)
                     {
                         RGBData.push(this.hexToRGB(forcedColor));
@@ -71,10 +69,7 @@ export default class TuyaVirtualDevice extends BaseClass
                     break;
             }
 
-            // Maybe this should be in the TuyaDevice
             let colorString = this.generateColorString(RGBData);
-
-            // Maybe this should be done by a global controller
             this.tuyaDevice.sendColors(colorString);
         }
     }
@@ -83,7 +78,7 @@ export default class TuyaVirtualDevice extends BaseClass
     {
         const RGBData = [];
     
-        for(let i = 0 ; i < this.ledPositions.length; i++){
+        for (let i = 0; i < this.ledPositions.length; i++) {
             const ledPosition = this.ledPositions[i];
             const color = device.color(ledPosition[0], ledPosition[1]);
             RGBData.push(color);
@@ -99,19 +94,20 @@ export default class TuyaVirtualDevice extends BaseClass
 
         if (spliceLength === 1)
         {
-            const [h1,s1,v1] = this.rgbToHsv(colors[0]);
+            const [h1, s1, v1] = this.rgbToHsv(colors[0]);
             let color = this.getW32FromHex(h1.toString(16), 2).toString(Hex) +
                         this.getW32FromHex(parseInt(s1 / 10).toString(16), 1).toString(Hex) +
                         this.getW32FromHex(parseInt(v1 / 10).toString(16), 1).toString(Hex);
 
             return color + "00000100";
-        } else
+        }
+        else
         {
             let colorArray = [];
 
             for (let color of colors)
             {
-                const [h,s,v] = this.rgbToHsv(color);
+                const [h, s, v] = this.rgbToHsv(color);
                 colorArray.push(
                     this.getW32FromHex(h.toString(16), 2).toString(Hex) +
                     this.getW32FromHex(s.toString(16), 2).toString(Hex) +
@@ -121,25 +117,15 @@ export default class TuyaVirtualDevice extends BaseClass
 
             let colorString = '';
 
-            for(let i = 1; i <= this.tuyaLeds.length; i++)
+            for (let i = 0; i < this.tuyaLeds.length; i++)
             {
-                // colorString += this.zeroPad(i, 2);
-                if (i <= 4) {
-                    colorString += '01';
-                } else if (i <= 8) {
-                    colorString += '02';
-                } else if (i <= 12) {
-                    colorString += '03';
-                } else if (i <= 16) {
-                    colorString += '04';
-                }
+                colorString += this.zeroPad(i + 1, 2);
             }
-    
+
             let spliceNumHex = this.getW32FromHex(spliceLength.toString(16), 2).toString(Hex);
             let colorValue = '0004' + colorArray.join('') + spliceNumHex + colorString;
-    
+
             return colorValue;
         }
-        
     }
 }
