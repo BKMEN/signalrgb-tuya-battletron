dce1eb7356445dc8c8c719692220f76a40487c6d
BKMEN <91731848+BKMEN@users.noreply.github.com>
2025-05-16 19:46:52 +0200

    Update DeviceList.test.js

--- Data/DeviceList.test.js
+++ Data/DeviceList.test.js
@@ -1,26 +1,14 @@
 export default {
-  "stmkcsykq3kheboa": {
-    leds: Array.from({ length: 16 }, (_, i) => i + 1),
-    name: "Battletron Light Bar"
-  },
-  "yrg6a649fkozp1hw": {
-    leds: [1],
-    name: "Battletron Ball Light"
-  },
-  "bfafad43febddb888apxbj": {
-    leds: Array.from({ length: 90 }, (_, i) => i + 1),
-    name: "Battletron Monitor Strip"
-  },
-  "bfbe7bd231444751090bsq": {
-    leds: Array.from({ length: 300 }, (_, i) => i + 1),
-    name: "Battletron Desk Strip 3m"
-  },
-  "bfbebb82be7220f985rawa": {
-    leds: Array.from({ length: 300 }, (_, i) => i + 1),
-    name: "Battletron Desk Strip (extra)"
-  },
-  "bfde5007394a05833ahsda": {
-    leds: Array.from({ length: 600 }, (_, i) => i + 1),
-    name: "Battletron Room Strip 10m"
-  }
+    "stmkcsykq3kheboa": {
+        leds: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+        name: "Battletron Light Bar"
+    },
+    "yrg6a649fkozp1hw": {
+        leds: [1],
+        name: "Battletron Ball Light"
+    },
+    "bfde5007394a05833ahsda": {
+        leds: Array.from({ length: 16 }, (_, i) => i + 1),
+        name: "Tira LED habitación 10m"
+    }
 };
